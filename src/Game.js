3611cac4fc548d6245be97927d47cedd30aeb184
Luke Argosino <luke.argosino@gmail.com>
2021-07-06 23:50:57 -0700

    git

--- src/Game.js
+++ src/Game.js
@@ -3,21 +3,29 @@ import React, {useState} from 'react'
 
 function Game() {
     const [counter, setCounter] = useState(0)
-    const handleClick = () => {
+    const handleDownClick = () => {
         imageClick()
         setCounter(counter + 1)
     }
+    
+    const handleUpClick = () => {
+        imageClickReset()
+    }
+
     return (
         <div className="Game">
             <div>{counter}</div>
-            <img id="mochi" src={window.location.origin + '/assets/mochi.png'} onClick={handleClick}/>
-            
+            <img id="mochi" src={window.location.origin + '/assets/mochi.png'} onMouseDown={handleDownClick} onMouseUp={handleUpClick}/>    
         </div>
     )
 }
 
 const imageClick = () => {
     document.getElementById("mochi").src='../assets/mochi_hit.png'
+}
+
+const imageClickReset = () => {
+    document.getElementById("mochi").src='../assets/mochi.png'
 }
 
 export default Game;
